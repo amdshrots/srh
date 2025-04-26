f4f69a4c8493c7be684c1fb0f2df2e28df376df3
amdshrots <159141089+amdshrots@users.noreply.github.com>
2025-04-26 09:54:37 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,49 @@
+
+sudo mdutil -i off -a
+
+#Create new account
+sudo dscl . -create /Users/kaiden
+sudo dscl . -create /Users/kaiden UserShell /bin/bash
+sudo dscl . -create /Users/kaiden RealName "Kaiden"
+sudo dscl . -create /Users/kaiden UniqueID 1001
+sudo dscl . -create /Users/kaiden PrimaryGroupID 80
+sudo dscl . -create /Users/kaiden NFSHomeDirectory /Users/vncuser
+sudo dscl . -passwd /Users/kaiden $1
+sudo dscl . -passwd /Users/kaiden $1
+sudo createhomedir -c -u kaiden > /dev/null
+
+#Enable VNC
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
+
+#VNC password - http://hints.macworld.com/article.php?story=20071103011608872
+echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+
+#Start VNC/reset changes
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
+
+
+#Enable Performance mode
+sudo nvram boot-args="serverperfmode=1 $(nvram boot-args 2>/dev/null | cut -f 2-)"
+
+#Reduce Motion and Transparency 
+defaults write com.apple.Accessibility DifferentiateWithoutColor -int 1
+defaults write com.apple.Accessibility ReduceMotionEnabled -int 1
+defaults write com.apple.universalaccess reduceMotion -int 1
+defaults write com.apple.universalaccess reduceTransparency -int 1
+defaults write com.apple.Accessibility ReduceMotionEnabled -int 1
+
+#Enable Multi-Session
+sudo /usr/bin/defaults write .GlobalPreferences MultipleSessionsEnabled -bool TRUE
+
+
+
+
+brew install --cask brave-browser
+brew install --cask chrome-remote-desktop-host
+brew install --cask microsoft-remote-desktop
+
+echo "Starting Pinggy tunnel..."
+ssh -p 443 -R0:localhost:5900 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 gtcxZbEfnfR+tcp@us.free.pinggy.io
+
