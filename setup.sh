47e67cbd18e924319a3b43eb7e9b6baaecb954fb
amdshrots <159141089+amdshrots@users.noreply.github.com>
2025-04-26 10:15:59 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,58 +1,52 @@
-# Disable Spotlight indexing
 sudo mdutil -i off -a
 
-# Create new account
-sudo dscl . -create /Users/kaiden
-sudo dscl . -create /Users/kaiden UserShell /bin/bash
-sudo dscl . -create /Users/kaiden RealName "Kaiden"
-sudo dscl . -create /Users/kaiden UniqueID 1001
-sudo dscl . -create /Users/kaiden PrimaryGroupID 80
-sudo dscl . -create /Users/kaiden NFSHomeDirectory /Users/kaiden
-sudo dscl . -passwd /Users/kaiden $1
-sudo createhomedir -c -u kaiden > /dev/null
+#Create new account
+sudo dscl . -create /Users/akhil
+sudo dscl . -create /Users/akhil UserShell /bin/bash
+sudo dscl . -create /Users/akhil RealName "Akhil"
+sudo dscl . -create /Users/akhil UniqueID 1001
+sudo dscl . -create /Users/akhil PrimaryGroupID 80
+sudo dscl . -create /Users/akhil NFSHomeDirectory /Users/vncuser
+sudo dscl . -passwd /Users/akhil $1
+sudo dscl . -passwd /Users/akhil $1
+sudo createhomedir -c -u akhil > /dev/null
 
-# Enable Screen Sharing
-sudo systemsetup -setremotelogin on
-sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
-sudo launchctl start com.apple.screensharing
-
-# Enable Remote Management
+#Enable VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
+
+#VNC password - http://hints.macworld.com/article.php?story=20071103011608872
+echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+
+#Start VNC/reset changes
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-# VNC password
-echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-# Enable Performance mode
+#Enable Performance mode
 sudo nvram boot-args="serverperfmode=1 $(nvram boot-args 2>/dev/null | cut -f 2-)"
 
-# Reduce Motion and Transparency
+#Reduce Motion and Transparency 
 defaults write com.apple.Accessibility DifferentiateWithoutColor -int 1
 defaults write com.apple.Accessibility ReduceMotionEnabled -int 1
 defaults write com.apple.universalaccess reduceMotion -int 1
 defaults write com.apple.universalaccess reduceTransparency -int 1
 defaults write com.apple.Accessibility ReduceMotionEnabled -int 1
 
-# Enable Multi-Session
+#Enable Multi-Session
 sudo /usr/bin/defaults write .GlobalPreferences MultipleSessionsEnabled -bool TRUE
+
+defaults write "Apple Global Domain" MultipleSessionsEnabled -bool true
+
+#Disable Screen-Lock
+defaults write com.apple.loginwindow DisableScreenLock -bool true
+
+defaults write com.apple.loginwindow AllowList -string '*'
 
 # Install applications
 brew install --cask brave-browser
 brew install --cask chrome-remote-desktop-host
 brew install --cask microsoft-remote-desktop
-
-# Ensure the VNC service is running correctly
-if sudo launchctl list | grep -q com.apple.RemoteManagement; then
-  echo "com.apple.RemoteManagement is already loaded."
-else
-  sudo launchctl bootstrap system /System/Library/LaunchDaemons/com.apple.RemoteManagement.plist
-  if [ $? -ne 0 ]; then
-    echo "Failed to load com.apple.RemoteManagement. Check the logs for more details."
-    exit 1
-  fi
-fi
 
 # Start Pinggy tunnel
 echo "Starting Pinggy tunnel..."
